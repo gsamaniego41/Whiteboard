ede0a0ba3a6764b81961a50489a7d9073e6b44a1
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-02-26 16:11:50 -0800

    session 1: basic react and react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, {Component} from "react";
 
+/* 
+1. Setup ReactRouter (it's already installed)
+2. Build a ChallengeList component that displays a list of individual ChallengeView component 
+  - Build a Route to ChallengeList
+  - For now, ChallengeView will only display title, description, and difficulty
+  - data in db folder
+3. {Stretch}
+*/
+
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <h1>Whiteboard App</h1>
+      </div>
+    );
   }
 }
-// Neat - Thomas
+
 export default App;
